053fdcc6eab8af254c22259bd46d17247b946a68
kenmuoghalu <kenmuoghalu944@gmail.com>
2025-09-04 13:24:18 +0100

    Commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generate random magic number between 1-100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        int guessCount = 0;
+
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            
+            // Add error handling for invalid input
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+
+            guessCount++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it in {guessCount} tries!");
+            }
+        }
     }
 }
